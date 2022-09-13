e485126139ce5bdd7793b77626ae8c53a5a6811a
Amanda Nguyen <amanda6@ualberta.ca>
2022-09-13 11:41:26 -0600

    added printing python script

--- lab-1/lab1.py
+++ lab-1/lab1.py
@@ -2,3 +2,4 @@ import requests
 
 print(requests.__version__)
 print(requests.get("http://www.google.com"))
+print(requests.get("https://raw.githubusercontent.com/amandanguyeen/cmput-404/main/lab-1/lab1.py").text)
